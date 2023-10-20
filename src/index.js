2ddd35a6488eaf6972feab896acbbd2de2193238
KundanGangurde <gangordaykondan@gmail.com>
2023-10-20 23:14:04 +0530

    near to finish

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }));
 
 // Connect to DATABASE
-const DATABASE_URL = "mongodb://localhost/subscribers";
+const DATABASE_URL = "mongodb+srv://gangordaykondan:gangordaykondan@cluster0.s34wiae.mongodb.net/";
 mongoose.connect(DATABASE_URL,{ useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection
 db.on('error', (err) => console.log(err))
